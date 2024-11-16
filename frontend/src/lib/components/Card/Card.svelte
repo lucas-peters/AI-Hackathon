51334dc7163c3815e8f93c1f823228492f0ce78d
Rawan Bekheet <rawanabbas@aucegypt.edu>
2024-11-16 14:41:19 -0800

    Add body to Card

--- frontend/src/lib/components/Card/Card.svelte
+++ frontend/src/lib/components/Card/Card.svelte
@@ -1,10 +1,15 @@
 <script lang="ts">
     export let image: string = "";
     export let title: string = "";
+    export let body: string = "";
 </script>
+
 <div class="max-w-sm rounded overflow-hidden shadow-lg">
-    <img src={image} alt="{title} Image" class="w-full">
+    <img src={image} alt="{title} Image" class="w-full" />
     <div class="px-4 py-4">
         <div class="font-bold text-xl mb-2">{title}</div>
+        {#if body}
+            <p class="text-gray-700 text-base">{body}</p>
+        {/if}
     </div>
 </div>
--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,22 +1,26 @@
 <script lang="ts">
-    import '../app.css'
-    import { Navbar } from '$lib/components/Navbar'
-    import { Card } from '$lib/components/Card'
-    import { Input } from '$lib/components/Input'
+    import "../app.css";
+    import { Navbar } from "$lib/components/Navbar";
+    import { Card } from "$lib/components/Card";
+    import { Input } from "$lib/components/Input";
     let image: string = "https://placehold.co/300";
-    let c = "w-full"
+    let body =
+        "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Doloremque amet aliquam itaque officiis dicta, assumenda rem modi fugiat maiores, beatae perspiciatis corrupti eligendi. Obcaecati fuga, nesciunt aspernatur odio perspiciatis ab!";
 </script>
+
 <Navbar />
 <div class="w-full flex justify-center items-center my-16">
     <h1 class="text-4xl">Welcome to Your Outfit of the Day!</h1>
 </div>
 <div class="container mx-auto flex flex-col justify-center items-center">
     <div class="flex gap-3 justify-center mb-10">
-        <Card {image} title="Shirt"/>
-        <Card {image} title="Pants"/>
-        <Card {image} title="Shoes"/>
+        <Card {image} title="Shirt" />
+        <Card {image} title="Pants" />
+        <Card {image} title="Shoes" />
     </div>
-    <div class="fixed bottom-0 w-full flex justify-center items-center bg-violet-50 py-4">
-        <Input/>
+    <div
+        class="fixed bottom-0 w-full flex justify-center items-center bg-violet-50 py-4"
+    >
+        <Input />
     </div>
 </div>
