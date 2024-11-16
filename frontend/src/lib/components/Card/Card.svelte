3bd91cb430141bd880ef578cb075b25bf75c48e6
Rawan Bekheet <rawanabbas@aucegypt.edu>
2024-11-16 13:50:17 -0800

    Fix Component Library

---
+++ frontend/src/lib/components/Card/Card.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+    export let image: string = "";
+    export let title: string = "";
+</script>
+<div class="max-w-sm rounded overflow-hidden shadow-lg">
+    <img src={image} alt="{title} Image" class="w-full">
+    <div class="px-4 py-4">
+        <div class="font-bold text-xl mb-2">{title}</div>
+    </div>
+</div>
--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,7 +1,15 @@
 <script lang="ts">
     import '../app.css'
     import { Navbar } from '$lib/components/Navbar'
+    import { Card } from '$lib/components/Card'
+    let image: string = "https://placehold.co/300";
 </script>
 <Navbar />
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<div class="w-full flex justify-center items-center my-16">
+    <h1 class="text-4xl">Welcome to Your Outfit of the Day!</h1>
+</div>
+<div class="container mx-auto flex gap-3 justify-center">
+    <Card {image} title="Shirt"/>
+    <Card {image} title="Pants"/>
+    <Card {image} title="Shoes"/>
+</div>
