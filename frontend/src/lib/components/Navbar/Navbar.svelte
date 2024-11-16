675e01c17944f418a3302d57a0559bc8db2b0fc0
Rawan Bekheet <rawanabbas@aucegypt.edu>
2024-11-16 14:35:33 -0800

    Add Input Bar

--- frontend/src/lib/components/Navbar/Navbar.svelte
+++ frontend/src/lib/components/Navbar/Navbar.svelte
@@ -1,9 +1,9 @@
-<nav class="bg-gray-800">
+<nav class="bg-violet-800">
   <div class="mx-auto px-2 sm:px-6 lg:px-8">
     <div class="relative flex h-16 items-center justify-between">
       <div class="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
         <div class="flex shrink-0 items-center">
-          <img class="h-8 w-auto" src="https://tailwindui.com/plus/img/logos/mark.svg?color=indigo&shade=500" alt="Your Company">
+          <img class="h-8 w-auto" src="https://img.logoipsum.com/292.svg" alt="Your Company">
         </div>
       </div>
       <div class="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -2,14 +2,21 @@
     import '../app.css'
     import { Navbar } from '$lib/components/Navbar'
     import { Card } from '$lib/components/Card'
+    import { Input } from '$lib/components/Input'
     let image: string = "https://placehold.co/300";
+    let c = "w-full"
 </script>
 <Navbar />
 <div class="w-full flex justify-center items-center my-16">
     <h1 class="text-4xl">Welcome to Your Outfit of the Day!</h1>
 </div>
-<div class="container mx-auto flex gap-3 justify-center">
-    <Card {image} title="Shirt"/>
-    <Card {image} title="Pants"/>
-    <Card {image} title="Shoes"/>
+<div class="container mx-auto flex flex-col justify-center items-center">
+    <div class="flex gap-3 justify-center mb-10">
+        <Card {image} title="Shirt"/>
+        <Card {image} title="Pants"/>
+        <Card {image} title="Shoes"/>
+    </div>
+    <div class="fixed bottom-0 w-full flex justify-center items-center bg-violet-50 py-4">
+        <Input/>
+    </div>
 </div>
