d3c7d58570d4a454c57fde7ca095acea2609dff3
Lucas <lppeters@ucdavis.edu>
2024-11-17 01:23:00 -0800

    Update main.py

--- FlaskApp/main.py
+++ FlaskApp/main.py
@@ -8,6 +8,6 @@ from app.chat import test_process_flask_request
 
 app = create_app()
 if __name__ == "__main__":
-    #app.run(debug=True)
-    json_obj = test_process_flask_request("I'm going to the Beach",'bob_gmail_com',{'time': '12:00:00', 'date': '12-24-2024'})
+    app.run(debug=True)
+    json_obj = test_process_flask_request("I'm going to a sex rave",'bob_gmail_com',{'time': '12:00:00', 'date': '12-24-2024'})
     print(json_obj)
