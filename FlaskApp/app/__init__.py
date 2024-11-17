a115a7de3e1a9623b987130886a79491b5dcb1b2
Lucas <lppeters@ucdavis.edu>
2024-11-16 22:54:24 -0800

    push

--- FlaskApp/app/__init__.py
+++ FlaskApp/app/__init__.py
@@ -1,9 +1,6 @@
 from flask import Flask
-from flask_login import LoginManager
 from flask_jwt_extended import JWTManager, create_access_token, jwt_required, get_jwt_identity
 import os
-
-login_manager = LoginManager()
 
 
 def create_app():
@@ -12,7 +9,6 @@ def create_app():
     app.config['SECRET_KEY'] = 'verysecurekey'
     app.config['JWT_SECRET_KEY'] = 'your_secret_key'  # Change this to a strong key
     jwt = JWTManager(app)
-    login_manager.init_app(app)
     
     from .chat import chat
     from .auth import auth
--- FlaskApp/app/chat.py
+++ FlaskApp/app/chat.py
@@ -1,33 +1,27 @@
 from flask import Blueprint, request, redirect, url_for, send_from_directory
-from .s3_queries import get_location, make_key_string
-from . import login_manager
+from s3_queries import get_location, make_key_string
+import os
+import sys
+dir = os.path.dirname(os.path.abspath(__file__))
+dir + '../../backend'
+sys.path.append(dir)
+from ...backend.prompt_processor import process_flask_request
 import json
 
 chat = Blueprint('views', __name__)
 
 @chat.route('/')
-def home(methods = ['POST']):
+def home(methods = ['GET', 'POST']):
     # check if user is logged_in
     # if not, redirect to login page
+    if request.method == 'POST':
+        json_req = request.get_json()
+        req = json.loads(json_req)
         
-    form.reque
-    if location is None:
-        location = get_location()
-    if time and date are null:
-        #get datetime
+        process_flask_request(req)
+    
+    if request.method == 'GET':
         pass
-    #key_string = make_key_string(user)
-    #tdl = {'location' : location, 'date': date, 'time' : time, 'prompt': prompt, 'user_id': key_string}
-    #json_data = json.dumps(tdl)
-    
-    process_flask_request(prommpt, make_key_string(email), tdl)
-    
-    
-    # receive time/date/location from frontend
-    # if any are null, I fill them in
-    # pack into json 
-    # send user_id, json, raw_promt to Shanaya
-    # wait for response??
         
     #return render_template('../../frontend/src/app.html')
     return send_from_directory('client/public', 'app.html')
